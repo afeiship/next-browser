60908097a37eba701de9000e088fe21c16062a90
afeiship <1290657123@qq.com>
2017-03-21 22:30:45 +0800

    update

--- src/next-browser.js
+++ src/next-browser.js
@@ -1,26 +1,40 @@
 (function(nx, global) {
 
   var navigator = global.navigator;
-  var Browser = nx.declare('nx.browser', {
+  var UA = navigator.userAgent;
+	var docStyle = document.documentElement.style;
+  var toString = Object.prototype.toString;
+  var OBJECT_OPERA = '[object Opera]';
+
+  var Browser = nx.declare('nx.Browser', {
     statics: {
       init: function() {
-        var u = navigator.userAgent,
-          app = navigator.appVersion;
         nx.mix(this, {
-          trident: u.indexOf('Trident') > -1, //IE内核
-          presto: u.indexOf('Presto') > -1, //opera内核
-          webkit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
-          gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
-          mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
-          ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
-          android: u.indexOf('Android') > -1 || u.indexOf('Adr') > -1, //android终端
-          iphone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
-          ipad: u.indexOf('iPad') > -1, //是否iPad
-          webapp: u.indexOf('Safari') == -1, //是否web应该程序，没有头部与底部
-          wechat: u.indexOf('MicroMessenger') > -1, //是否微信 （2015-01-22新增）
-          qq: u.match(/\sQQ/i) == " qq", //是否QQ,
-          language: (navigator.browserLanguage || navigator.language).toLowerCase()
+          mobile: !!UA.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+          ios: !!UA.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+          android: UA.indexOf('Android') > -1 || UA.indexOf('Adr') > -1, //android终端
+          iphone: UA.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+          ipad: UA.indexOf('iPad') > -1, //是否iPad
+          webapp: UA.indexOf('Safari') == -1, //是否web应该程序，没有头部与底部
+          wechat: UA.indexOf('MicroMessenger') > -1, //是否微信 （2015-01-22新增）
+          qq: UA.match(/\sQQ/i) == " qq", //是否QQ
         });
+      },
+      engine: function(){
+        if (global.opera && toString.call(opera) === OBJECT_OPERA) {
+          return 'presto';
+        } else if ('MozAppearance' in docStyle) {
+          return 'gecko';
+        } else if ('WebkitAppearance' in docStyle) {
+          return 'webkit';
+        } else if (typeof navigator.cpuClass === 'string') {
+          return 'trident';
+        }else{
+          return 'unknown';
+        }
+      },
+      language: function(){
+        return (navigator.browserLanguage || navigator.language).toLowerCase();
       }
     }
   });
